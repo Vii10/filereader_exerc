c8e65f8a46a669f714f0b63c796af2a32294bbf5
The John! <jdworks1998@gmail.com>
2022-09-20 23:52:23 -0300

    FileReader Exercise with CompareTo Interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,23 +8,26 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
+import model.entities.Employee;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		String path = "C:\\Users\\Usuário\\eclipse-workspace\\filereader_exerc\\in.txt";
-		List<String> list = new ArrayList<>();
+		List<Employee> list = new ArrayList<>();
 		
 		try(BufferedReader br = new BufferedReader(new FileReader(path))){
 			String names = br.readLine();
 			while(names != null) {
-				list.add(names);
+				String[] fields = names.split(",");
+				list.add(new Employee(fields[0], Double.parseDouble(fields[1]))); //Convertendo o string do arquivo para um número
 				names = br.readLine();
 			}
 			Collections.sort(list); //Aqui nós arranjamos a lista por nome caso não haja uma classe específica para o objeto da lista
-			for(String n : list) {
-				System.out.println(n);
+			for(Employee n : list) {
+				System.out.println(n.getName() + ", " + n.getSalary());
 			}
 		}
 		catch(IOException e) {
